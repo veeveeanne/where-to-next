d4d79d644025cacdb0109244bc9159bfdd796527
Vivian Wang <vivian.emmenuel.wang@gmail.com>
2020-04-23 07:47:06 -0400

    established static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root 'homes#index'
+  root 'static_pages#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/destinations', to: 'homes#index'
+  get '/destinations', to: 'static_pages#index'
 
   namespace :api do
     namespace :v1 do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Destination.create(name: "Lincoln Center", state: "New York")
+Destination.create(name: "Art Institute of Chicago", state: "Illinois")
+Destination.create(name: "Gateway Arch", state: "Missouri")
